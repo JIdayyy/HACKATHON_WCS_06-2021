2315d833a68c69a277456144339f3279519c5720
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-29 01:12:35 +0200

    fix

--- pages/squad.tsx
+++ pages/squad.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
-
-import { fakeSquad } from '../fakeData';
-import Group_Card from '../components/Group_Card';
-function squad(): JSX.Element {
-  return (
-    <div className="h-screen overflow-y-auto w-full  ">
-      <div className=" flex flex-col items-center w-full">
-        <div className="ease-in-out flex mb-40 justify-center sm:justify-center flex-wrap">
-          {fakeSquad.map((group, index) => {
-            return <Group_Card key={index} name={fakeSquad[0].name} buisness={fakeSquad[0].buisness} description={fakeSquad[0].description} />;
 import { fakeSquad } from '../fakeData';
 import Group_Card from '../components/Group_Card';
 import { useQuery } from '@apollo/client';
 import { getSquads } from '../apollo/squadQueries';
+
+
+
 function squad() {
   const { data, loading } = useQuery(getSquads);
   console.log(data);
