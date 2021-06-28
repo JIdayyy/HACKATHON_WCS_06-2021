9cb0417f2d0d8561b00cede10ec29ebc903ce2a4
Thomas <barrialthomas@gmail.com>
2021-06-28 22:52:05 +0200

    mobile fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,15 +18,15 @@ export default function Home(): JSX.Element {
       <div className="text-white w-screen mt-20 flex flex-col justify-center items-center">
         <div className="font-Open text-center">
           <h1 className="sm:text-9xl text-6xl text-center font-bold">fiverr.squad</h1>
-          <h2 className="mt-12 sm:text-5xl text-3xl font-bold">Find your perfect squad and speed up your projects.</h2>
+          <h2 className="mt-12 sm:text-5xl mx-4 text-2xl font-bold">Find your perfect squad and speed up your projects.</h2>
         </div>
-        <div className="text-center font-Montserrat mt-16">
-          <h2 className="sm:text-3xl text-2xl font-light">You are aproject Manager or a freelancer</h2>
-          <h3 className="mt-5 sm:text-2xl text-lg font-light">Find the team you need already build and ready to work on your dream project. </h3>
+        <div className="text-center font-Montserrat mx-4 mt-16">
+          <h2 className="sm:text-3xl text-xl font-light">You are aproject Manager or a freelancer</h2>
+          <h3 className="mt-5 sm:text-2xl text-sm font-light">Find the team you need already build and ready to work on your dream project. </h3>
         </div>
-        <div className="flex flex-col sm:flex-row sm:w-4/12 mt-20 justify-between">
+        <div className="flex flex-col sm:flex-row sm:w-4/12 sm:mt-20 mt-10 sm:justify-between">
           <Link href="/squad">
-            <a className="bg-pink shadow-buttonShadow mt-5 px-8 py-2 rounded-md">Find Your Squad</a>
+            <a className="bg-pink shadow-buttonShadow  px-8 py-2 rounded-md">Find Your Squad</a>
           </Link>
           <button className="bg-pink shadow-buttonShadow mt-5 px-8 py-2 rounded-md">Hire Your Squad</button>
         </div>
