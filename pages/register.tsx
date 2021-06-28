eef2bb0782b926cce33ccc85ff033988592116c5
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-28 15:39:17 +0200

    ts and co

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,6 +1,6 @@
 import RegisterForm from '../components/RegisterForm'
 
-export default function register(params) {
+export default function register():JSX.Element  {
     return(
         <div className="w-full bg-blue-900 bg-opacity-80 h-screen flex flex-col items-center justify-center align-middle">
             <RegisterForm />
