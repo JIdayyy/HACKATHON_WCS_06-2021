a86de30b19d50ce34fd43f02f49be8ee3904866a
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-28 16:19:52 +0200

    group_card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-
+import Group_Card from '../components/Group_Card'
 export default function Home():JSX.Element {
   return (
     <div className="flex flex-col w-full  items-center justify-center min-h-screen py-2">
-      
+      <Group_Card/>
     </div>
   )
 }
