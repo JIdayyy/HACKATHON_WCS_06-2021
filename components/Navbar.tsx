a736df01d0302fe3351637a8fa1f24082cc1d597
Thomas <barrialthomas@gmail.com>
2021-06-29 01:45:04 +0200

    MAj

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,16 @@
 import Image from 'next/image';
 import Log from '../public/LogIn.svg';
-import SearchBar from '../components/SearchBar';
+import SearchBar from './SearchBar';
 
 export default function Navbar() {
   return (
-    <div className=" flex flex-col sm:flex-row text-white sm:h-24 h-32  font-Open z-20 px-3 sm:px-5 sm:py-6 py-5 justify-between align-middle sm:items-center w-full">
-      <div className="flex items-center justify-between">
-        <h1 className="sm:text-4xl flex text-2xl font-bold">Fiverr. Squad</h1>
-        <div className="flex">
-          <h1 className="sm:text-xl font-bold mr-2">Thomas Barrial</h1>
-          <Image className="" src={Log} height={25} width={25} alt="log" />
-        </div>
-      </div>
+    <div className="flex flex-col static sm:flex-row text-white sm:h-24 h-full font-Open z-20 px-3 sm:px-5 sm:py-6 py-5  justify-between align-middle items-center w-full">
+      <div className="sm:text-4xl text-4xl font-bold">Fiverr. Squad</div>
       <SearchBar />
+      <div className="flex items-center">
+        <h1 className="sm:text-xl font-bold mr-2 ">Thomas Barrial</h1>
+        <Image className="" src={Log} height={25} width={25} alt="log" />
+      </div>
     </div>
   );
 }
--- pages/squad.tsx
+++ pages/squad.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Sidebar from '../components/Sidebar';
 
 import { fakeSquad } from '../fakeData';
 import Group_Card from '../components/Group_Card';
@@ -7,7 +6,7 @@ function squad(): JSX.Element {
   return (
     <div className="h-screen overflow-y-auto w-full  ">
       <div className=" flex flex-col items-center w-full">
-        <div className="ease-in-out flex mb-40 justify-center sm:justify-center flex-wrap">
+        <div className="ease-in-out flex mb-60 justify-center sm:justify-center flex-wrap">
           {fakeSquad.map((group, index) => {
             return <Group_Card key={index} name={fakeSquad[0].name} buisness={fakeSquad[0].buisness} description={fakeSquad[0].description} />;
           })}
