6b5529107e52f382dfc5c0efb94934503a0c6202
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-28 15:22:21 +0200

    ini

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 import 'tailwindcss/tailwind.css'
 import Layout from '../components/Layout'
-
-
+import { QueryClient, QueryClientProvider } from 'react-query'
+const queryClient = new QueryClient()
 
 
 function MyApp({ Component, pageProps }) {
   return (
+        <QueryClientProvider client={queryClient}>
         <Layout page="Home">
           <Component {...pageProps} />
-          glzirngôizhgoih
         </Layout>
+        </QueryClientProvider>
   )
 }
 
