302cd50c724cd97836caf70e7eb07b2773e1704d
Thomas <barrialthomas@gmail.com>
2021-06-28 23:12:15 +0200

    fix

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,7 +2,7 @@ import LoginForm from '../components/LoginForm';
 
 export default function Login(): JSX.Element {
   return (
-    <div className="w-full bg-backGround absolute h-screen flex flex-col items-center justify-center align-middle">
+    <div className="w-full bg-backGround absolute top-0 h-screen flex flex-col items-center justify-center align-middle">
       <LoginForm />
     </div>
   );
