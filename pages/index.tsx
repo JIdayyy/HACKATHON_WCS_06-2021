2a30b1e01c0066b83911de87a7b8c0845451a1cd
Thomas <barrialthomas@gmail.com>
2021-06-28 18:01:13 +0200

    style

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,9 @@ export default function Home(): JSX.Element {
           <h1 className="text-9xl text-center font-bold">fiverr.squad</h1>
           <h2 className="text-4xl font-bold">Find your perfect squad and speed up your projects.</h2>
         </div>
-        <div className="text-center">
-          <h2>You are aproject Manager or a freelancer</h2>
-          <h3>Find the team you need already build and ready to work on your dream project. </h3>
+        <div className="text-center font-Montserrat mt-10">
+          <h2 className="text-3xl font-light">You are aproject Manager or a freelancer</h2>
+          <h3 className="text-2xl font-light">Find the team you need already build and ready to work on your dream project. </h3>
         </div>
       </div>
     </div>
