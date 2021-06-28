1e9b09f24b15e9d802e4bbfbc4281d510624e332
Thomas <barrialthomas@gmail.com>
2021-06-28 22:44:08 +0200

    fINISH

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -17,43 +17,44 @@ export default function LoginForm(): JSX.Element {
   const [show, setShow] = useState(false);
 
   return (
-    <div className="w-screen h-screen absolute bg-backGround bg-cover top-0 z-50">
-      <div className="w-screen h-screen flex flex-col justify-center items-center bg-black bg-opacity-50">
-        <div className="w-2/3 flex flex-col  rounded-lg p-8 md:p-16 bg-backGround bg-cover sm:w-6/12 shadow-inputShadow">
-          <div className="flex justify-end -m-6 ">
-            <Link href="/">
-              <Image src={close} width={20} height={20} alt="close" />
-            </Link>
+    <div className="w-screen h-screen absolute bg-backGround bg-no-repeat bg-cover z-50">
+      <div className="w-screen h-screen flex flex-col justify-center absolute items-center bg-black bg-opacity-50">
+        <div className="w-11/12 flex flex-col  rounded-lg p-8 md:p-12 bg-backGround bg-cover sm:w-6/12 shadow-inputShadow">
+          <div className="flex w-full justify-between">
+            <div className="flex items-start justify-between w-full">
+              <div className="flex flex-col  ">
+                <p className="text-white font-Open text-4xl font-bold">Login</p>
+                <p className="text-white text-xl">Join and work with the fiver freelance community</p>
+              </div>
+              <Link href="/">
+                <Image className="cursor-pointer" src={close} width={20} height={20} alt="close" />
+              </Link>
+            </div>
           </div>
 
-          <div className="flex flex-col mb-7">
-            <p className="text-white text-lg font-bold ">Login</p>
-            <p className="text-white">Join and work with the fiver freelance community</p>
-          </div>
-
-          <form className="flex flex-col space-y-3  " onSubmit={handleSubmit(onSubmit)}>
+          <form className="flex font-Open flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
             <p className="text-white">Email</p>
             <input
-              className="rounded-md bg-grayinput bg-opacity-30 shadow-inputShadow p-1"
+              className="rounded-md text-white focus:ouline-none bg-grayinput bg-opacity-30 shadow-inputShadow p-2"
               type="text"
               placeholder="email@email.com"
               {...register('email', {})}
             />
-            <p className="text-white ">Password</p>
+            <p className="text-white mt-3">Password</p>
             <input
-              className="rounded-md bg-grayinput bg-opacity-30 shadow-inputShadow p-1"
+              className="rounded-md text-white bg-grayinput focus:outline-none bg-opacity-30 shadow-inputShadow p-2"
               type="text"
               placeholder="password"
               {...register('password', {})}
             />
-            <p className="text-white ">Confirm Password</p>
+            <p className="text-white mt-3">Confirm Password</p>
             <input
-              className="rounded-md bg-grayinput bg-opacity-30 shadow-inputShadow p-1"
+              className="rounded-md text-white bg-grayinput bg-opacity-30 focus:outline-none shadow-inputShadow p-2"
               type="text"
               placeholder="confirm password"
               {...register('confirm password', {})}
             />
-            <input className="rounded-md p-1 text-white bg-buttonBlue shadow-inputShadow" type="submit" value="Connect" />
+            <input className="rounded-md p-1 mt-5 text-white bg-buttonBlue focus:outline-none shadow-inputShadow" type="submit" value="Connect" />
             <div className="flex flex-col text-center mt-2 mb-8 text-white">
               <p className="text-xs">
                 {"Don't have an account? "}
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,8 @@
-import LoginForm from "../components/LoginForm";
+import LoginForm from '../components/LoginForm';
 
 export default function Login(): JSX.Element {
   return (
-    <div className="w-full bg-backGround h-screen flex flex-col items-center justify-center align-middle">
+    <div className="w-full bg-backGround absolute h-screen flex flex-col items-center justify-center align-middle">
       <LoginForm />
     </div>
   );
