0ad63b82759651ad331056035c87ee87a59b2bc3
viccabezos <cabezosvic@gmail.com>
2021-06-29 00:03:10 +0200

    connect signup to navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import Group_Card from '../components/Group_Card';
-import SearchBar from '../components/SearchBar';
-import Image from 'next/image';
-import Log from '../public/LogIn.svg';
-import Link from 'next/link';
+import Group_Card from "../components/Group_Card";
+import SearchBar from "../components/SearchBar";
+import Image from "next/image";
+import Log from "../public/LogIn.svg";
+import Link from "next/link";
 export default function Home(): JSX.Element {
   const ressource = [];
   return (
@@ -11,23 +11,39 @@ export default function Home(): JSX.Element {
         <Link href="/login">
           <button className=" mr-2 text-xl">LogIn /</button>
         </Link>
-        <button className=" mr-2 text-xl">SignIn</button>
+        <Link href="/signup">
+          <button className=" mr-2 text-xl">Sign up</button>
+        </Link>
+
         <Image className="" src={Log} height={20} width={20} alt="log" />
       </div>
       <div className="text-white w-screen mt-20 flex flex-col justify-center items-center">
         <div className="font-Open text-center">
-          <h1 className="sm:text-9xl text-6xl text-center font-bold">fiverr.squad</h1>
-          <h2 className="mt-12 sm:text-5xl mx-4 text-2xl font-bold">Find your perfect squad and speed up your projects.</h2>
+          <h1 className="sm:text-9xl text-6xl text-center font-bold">
+            fiverr.squad
+          </h1>
+          <h2 className="mt-12 sm:text-5xl mx-4 text-2xl font-bold">
+            Find your perfect squad and speed up your projects.
+          </h2>
         </div>
         <div className="text-center font-Montserrat mx-4 mt-16">
-          <h2 className="sm:text-3xl text-xl font-light">You are aproject Manager or a freelancer</h2>
-          <h3 className="mt-5 sm:text-2xl text-sm font-light">Find the team you need already build and ready to work on your dream project. </h3>
+          <h2 className="sm:text-3xl text-xl font-light">
+            You are aproject Manager or a freelancer
+          </h2>
+          <h3 className="mt-5 sm:text-2xl text-sm font-light">
+            Find the team you need already build and ready to work on your dream
+            project.{" "}
+          </h3>
         </div>
         <div className="flex flex-col sm:flex-row sm:w-4/12 sm:mt-20 mt-10 sm:justify-between">
           <Link href="/squad">
-            <a className="bg-pink shadow-buttonShadow mt-5 px-8 py-2 rounded-md">Find Your Squad</a>
+            <a className="bg-pink shadow-buttonShadow mt-5 px-8 py-2 rounded-md">
+              Find Your Squad
+            </a>
           </Link>
-          <button className="bg-pink shadow-buttonShadow mt-5 px-8 py-2 rounded-md">Hire Your Squad</button>
+          <button className="bg-pink shadow-buttonShadow mt-5 px-8 py-2 rounded-md">
+            Hire Your Squad
+          </button>
         </div>
       </div>
     </div>
