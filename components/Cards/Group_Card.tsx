afd060b1b35ebab666c2d3ded4a46361e80d3c10
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-29 01:44:14 +0200

    init

--- components/Cards/Group_Card.tsx
+++ components/Cards/Group_Card.tsx
@@ -1,19 +1,21 @@
 import Image from 'next/image';
-import { fakeSquad } from '../fakeData';
+
 
 interface IProps {
   name: string;
   buisness: string;
   description: string;
+  job:string;
 }
 
-export default function Group_Card({ name, buisness, description }: IProps) {
+export default function Freelance_Card({ name, buisness, description,job }: IProps) {
   return (
     <div className=" transition duration-500 w-11/12 sm:w-60 h-96 bg-white bg-blur-xl bg-opacity-20 m-4 flex flex-col items-center shadow-inputShadow align-middle justify-between p-4 rounded-xl text-white transform hover:-translate-y-1 hover:scale-110">
       <Image width={120} height={230} src="/profile_placeholder.png" />
       <div className="w-full h-full flex flex-col items-left align-middle justify-center">
         <div className="text-2xl">{name}</div>
         <div className="text-xl">{buisness}</div>
+        <div className="text-xl">{job}</div>
         <div className="text-xs">{description}</div>
       </div>
       <div className="w-full">
--- pages/squad.tsx
+++ pages/squad.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Sidebar from '../components/Sidebar';
 import { fakeSquad } from '../fakeData';
-import Group_Card from '../components/Group_Card';
+import Group_Card from '../components/Cards/Group_Card'
 import { useQuery } from '@apollo/client';
 import { getSquads } from '../apollo/squadQueries';
 
