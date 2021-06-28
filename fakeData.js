86f2f4640654f73461e40b1429c9101a1cb247c8
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-28 22:20:00 +0200

    sidebar ok

---
+++ fakeData.js
@@ -0,0 +1,176 @@
+export  const fakeSquad = [{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+},{
+    name: "Space-X",
+    buisness: "Web Development",
+    description:"Ma super description",
+
+}]
