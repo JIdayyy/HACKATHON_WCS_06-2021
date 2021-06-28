206c9e90267cdd436512f8f9790e90aa1f671bd2
viccabezos <cabezosvic@gmail.com>
2021-06-28 19:30:44 +0200

    form login

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -10,25 +10,46 @@ export default function LoginForm(): JSX.Element {
   console.log(errors);
 
   return (
-    <div className="w-2/3 ">
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col  ">
+    <div className="w-2/3 h-3/5 rounded-lg p-16 bg-backGround  ">
+      <p className="flex justify-end text-white text-xs ">X</p>
+      <div className="flex flex-col mb-7">
+        <p className="text-white text-lg font-bold ">Login</p>
+        <p className="text-white">
+          Join and work with the fiver freelance community
+        </p>
+      </div>
+
+      <form
+        className="flex flex-col space-y-3  "
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <p className="text-white">Email</p>
         <input
+          className="rounded-md bg-grayinput bg-opacity-30 shadow-inputShadow p-1"
           type="text"
           placeholder="email@email.com"
           {...register("email", {})}
         />
+        <p className="text-white ">Password</p>
         <input
+          className="rounded-md bg-grayinput bg-opacity-30 shadow-inputShadow p-1"
           type="text"
           placeholder="password"
           {...register("password", {})}
         />
+        <p className="text-white ">Confirm Password</p>
         <input
+          className="rounded-md bg-grayinput bg-opacity-30 shadow-inputShadow p-1"
           type="text"
           placeholder="confirm password"
           {...register("confirm password", {})}
         />
 
-        <input type="submit" />
+        <input
+          className="rounded-md p-1 text-white bg-buttonBlue shadow-inputShadow"
+          type="submit"
+          value="Connect"
+        />
       </form>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        Montserrat: ['Montserrat', 'sans-serif'],
-        Open: ['Open', 'Sans-serif'],
+        Montserrat: ["Montserrat", "sans-serif"],
+        Open: ["Open", "Sans-serif"],
       },
       backgroundImage: {
         backGround: 'url("/images/bg.webp")',
+      },
+
+      colors: {
+        grayinput: "#C4C4C4",
+        buttonBlue: "#4AC9FF",
+      },
+      boxShadow: {
+        inputShadow: "10px 10px 20px rgba(0, 0, 0, 0.2)",
       },
     },
   },
