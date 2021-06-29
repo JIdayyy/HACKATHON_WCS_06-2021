65cf6d83d18d48fe8f43b1a1fa30db0d6a74363a
viccabezos <cabezosvic@gmail.com>
2021-06-29 11:40:21 +0200

    structure page

--- pages/profil.tsx
+++ pages/profil.tsx
@@ -1,7 +1,46 @@
 export default function Profil() {
   return (
-    <div>
-      <div>Profil page</div>
+    <div className="flex flex-col w-full  text-white items-center overflow-y-auto">
+      <div className="flex flex-col w-11/12 md:w-10/12 lg:w-8/12">
+        <div className="">
+          <h1>Welcome to Julia's profile</h1>
+        </div>
+        <div className="flex flex-col">
+          <div className="h-40 w-40 bg-gray-500">photo</div>
+          <div className="">
+            INFOS
+            <h2>Julia Matth</h2>
+            <p>Translater English/ Polish</p>
+            <p>
+              Working in <a href="/squadProfil">IA translater Squad</a>
+            </p>
+            <a href="">Fiverr Profile</a>
+          </div>
+        </div>
+        <div className="text-justify">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio at
+            porttitor elit, luctus. Leo, dui egestas sit mi tellus et sed
+            fermentum mauris. Velit morbi consectetur dolor elementum. Eu
+            facilisis condimentum ligula enim. Leo aliquam amet nunc maecenas.
+            Justo iaculis aliquam quisque tempor. Nibh venenatis dolor, ut
+            mauris sit ipsum malesuada. Lobortis interdum erat scelerisque
+            hendrerit duis. Senectus elit justo, purus fringilla faucibus sit mi
+            lorem. Amet faucibus sed venenatis purus risus id cursus eget.
+            Gravida eget auctor euismod risus ultrices. Aliquet volutpat risus
+            quis aenean eu. Amet vitae aliquam iaculis integer amet, erat.{" "}
+          </p>
+        </div>
+        <div className="">
+          <button className="rounded-md p-1 mt-5 text-white bg-buttonBlue focus:outline-none shadow-inputShadow">
+            Contact
+          </button>
+        </div>
+        <div className="">
+          <h2>Projects</h2>
+          <div>cards</div>
+        </div>
+      </div>
     </div>
   );
 }
--- pages/squad/index.tsx
+++ pages/squad/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { fakeSquad } from '../../fakeData';
-import Group_Card from '../../components/Group_Card';
-import { useQuery } from '@apollo/client';
-import { getSquads } from '../../apollo/squadQueries';
+import React from "react";
+import { fakeSquad } from "../../fakeData";
+import Group_Card from "../../components/Group_Card";
+import { useQuery } from "@apollo/client";
+import { getSquads } from "../../apollo/squadQueries";
 
 function squad() {
   const { data, loading } = useQuery(getSquads);
@@ -11,8 +11,14 @@ function squad() {
     <div className="h-screen overflow-y-auto w-full  ">
       <div className=" flex flex-col items-center w-full">
         <div className="ease-in-out flex mb-60 justify-center sm:justify-center flex-wrap">
-          {fakeSquad.map((group, index) => {
-            return <Group_Card key={index} name={group.name} description={group.description} />;
+          {data?.Squad.map((group, index) => {
+            return (
+              <Group_Card
+                key={index}
+                name={group.name}
+                description={group.description}
+              />
+            );
           })}
         </div>
       </div>
