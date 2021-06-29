37cd4bf3c7c2888a69bfdf618ad1d8ce4b209992
viccabezos <cabezosvic@gmail.com>
2021-06-29 02:57:10 +0200

    creation branch et page

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,12 +1,17 @@
 export default function LoginForm() {
-    return(
-        
-        <div className="text-white p-10 rounded-md">
-            <label>Email </label>
-            <input className="w-full h-11 bg-gray-500 my-4 px-4 rounded-md"   type="text"/>
-            <label>Password: </label>
-            <input className="w-full h-11 bg-gray-500 my-4 px-4 rounded-md"   type="text"/>
-            <button className="py-2 border rounded-md px-4">LOGIN</button>
-        </div>
-    )
-};
+  return (
+    <div className="text-white p-10 rounded-md">
+      <label>Email </label>
+      <input
+        className="w-full h-11 bg-gray-500 my-4 px-4 rounded-md"
+        type="text"
+      />
+      <label>Password: </label>
+      <input
+        className="w-full h-11 bg-gray-500 my-4 px-4 rounded-md"
+        type="text"
+      />
+      <button className="py-2 border rounded-md px-4">LOGIN</button>
+    </div>
+  );
+}
