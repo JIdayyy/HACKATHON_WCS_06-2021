517c9f5df11dae0263ffeea1aa07c74bc1d0f40c
Thomas <barrialthomas@gmail.com>
2021-06-29 02:53:09 +0200

    maj

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,26 @@
 import Image from 'next/image';
 import Log from '../public/LogIn.svg';
 import SearchBar from './SearchBar';
+import burger from '../public/burger.svg';
+import { SetStateAction, Dispatch } from 'react';
+import { set } from 'react-hook-form';
 
-export default function Navbar() {
+export default function Navbar({ setIsSideBarVisible }: { setIsSideBarVisible: Dispatch<SetStateAction<boolean>> }) {
+  const handleClick = () => {
+    setIsSideBarVisible(true);
+  };
   return (
     <div className="flex flex-col static sm:flex-row text-white sm:h-24 h-full font-Open z-20 px-3 sm:px-5 sm:py-6 py-5  justify-between align-middle items-center w-full">
       <div className="sm:text-4xl text-4xl font-bold">Fiverr. Squad</div>
       <SearchBar />
-      <div className="flex items-center">
-        <h1 className="sm:text-xl font-bold mr-2 ">Thomas Barrial</h1>
-        <Image className="" src={Log} height={25} width={25} alt="log" />
+      <div className="flex items-end sm:items-center justify-between w-full sm:justify-end sm:w-8/12 px-2 mt-10">
+        <div className="flex items-end">
+          <h1 className="sm:text-xl font-bold mr-2 ">Thomas Barrial</h1>
+          <Image className="" src={Log} height={25} width={25} alt="log" />
+        </div>
+        <button onClick={handleClick}>
+          <Image src={burger} alt="burger" />{' '}
+        </button>
       </div>
     </div>
   );
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,26 @@
-export default function Sidebar() {
-  return <div className="w-sideBar z-50 transform hidden sm:flex  bg-opacity-75 bg-black"></div>;
+import { Dispatch, SetStateAction, useState } from 'react';
+import Image from 'next/image';
+import close from '../public/images/close.svg';
+
+interface IProps {
+  isSideBarVisible: boolean;
+  setIsSideBarVisible: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function Sidebar({ isSideBarVisible, setIsSideBarVisible }: IProps) {
+  const handleClick = () => {
+    setIsSideBarVisible(false);
+  };
+  return (
+    <div
+      className={
+        isSideBarVisible
+          ? 'w-screen h-screen absolute top-0  left-0 z-50 transform flex bg-opacity-75 bg-black'
+          : 'w-sideBar z-50 transform hidden sm:flex  bg-opacity-75 bg-black'
+      }>
+      <button onClick={handleClick}>
+        <Image src={close} height={25} width={25} alt="cross" />
+      </button>
+    </div>
+  );
 }
