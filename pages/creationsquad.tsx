4129c2370d00365fb00f5b61b7c01150141fa721
viccabezos <cabezosvic@gmail.com>
2021-06-29 03:20:57 +0200

    skelet return

--- pages/creationsquad.tsx
+++ pages/creationsquad.tsx
@@ -2,7 +2,7 @@ import Sidebar from "../components/Sidebar";
 
 export default function CreationSquad(): JSX.Element {
   return (
-    <div>
+    <div className="flex flex-col text-white">
       <div>
         <h1>Create your Squad</h1>
       </div>
