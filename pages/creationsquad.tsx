d3c3b992d4b269945587a60d1db438c4454705c6
viccabezos <cabezosvic@gmail.com>
2021-06-29 09:24:51 +0200

    responsive to form

--- pages/creationsquad.tsx
+++ pages/creationsquad.tsx
@@ -14,11 +14,11 @@ export default function CreationSquad(): JSX.Element {
   console.log(errors);
 
   return (
-    <div className="flex flex-col text-white ">
-      <div className="flex flex-col justify-center text-center">
+    <div className="flex flex-col w-full  text-white items-center overflow-y-auto  ">
+      <div className="flex flex-col w-11/12 md:w-10/12 lg:w-8/12">
         <h1>Create your Squad</h1>
         <form
-          className="flex font-Open flex-col mt-3"
+          className=" font-Open  mt-3 flex flex-col"
           onSubmit={handleSubmit(onSubmit)}
         >
           <p className="text-white">Squad name</p>
@@ -28,14 +28,14 @@ export default function CreationSquad(): JSX.Element {
             placeholder="Space rocket"
             {...register("name", {})}
           />
-          <p className="text-white">Welcoming text</p>
+          <p className="text-white mt-3">Welcoming text</p>
           <input
             className="rounded-md text-white focus:ouline-none bg-grayinput bg-opacity-30 shadow-inputShadow p-2"
             type="text"
             placeholder="Your Project to the moon and back"
             {...register("name", {})}
           />
-          <p className="text-white">member n*</p>
+          <p className="text-white mt-3">member n*</p>
           <input
             className="rounded-md text-white focus:ouline-none bg-grayinput bg-opacity-30 shadow-inputShadow p-2"
             type="number"
@@ -47,7 +47,7 @@ export default function CreationSquad(): JSX.Element {
             className="h-36 rounded-md text-white focus:ouline-none bg-grayinput bg-opacity-30 shadow-inputShadow p-2"
             {...register("message", { maxLength: 500 })}
           />
-          <p className="text-white">Squad Image</p>
+          <p className="text-white mt-3">Squad Image</p>
           <input
             className="rounded-md text-white focus:ouline-none bg-grayinput bg-opacity-30 shadow-inputShadow p-2"
             type="text"
@@ -56,10 +56,16 @@ export default function CreationSquad(): JSX.Element {
           />
           <p className="text-white">Contact Email</p>
           <input
-            className="rounded-md text-white focus:ouline-none bg-grayinput bg-opacity-30 shadow-inputShadow p-2"
+            className="rounded-md  mt-3 text-white focus:ouline-none bg-grayinput bg-opacity-30 shadow-inputShadow p-2"
             type="text"
             placeholder="email@email.com"
             {...register("email", {})}
+          />
+          <input
+            className="rounded-md p-1 mt-5 w-20 text-white bg-buttonBlue focus:outline-none shadow-inputShadow"
+            type="submit"
+            value="Save"
+            onClick={handleSubmit(onSubmit)}
           />
         </form>
       </div>
