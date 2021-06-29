bc8f552e1db43e3223e06f0ea0fb60053130033b
Thomas <barrialthomas@gmail.com>
2021-06-29 12:49:44 +0200

    aj

--- components/Freelance_Card.tsx
+++ components/Freelance_Card.tsx
@@ -1,25 +1,32 @@
 import Image from 'next/image';
+import { useQuery } from '@apollo/client';
+import { getOneUser } from '../apollo/userQueries';
 import { fakeSquad } from '../fakeData';
 
 interface IProps {
-  name: string;
-  business: string;
-  description: string;
-  job: string,
+  id: string;
 }
 
-export default function Freelance_Card({ name, business, description,job }: IProps) {
+export default function Freelance_Card({ id }: IProps) {
+  console.log(id);
+  // if (id === undefined) {
+  //   <p>error</p>;
+  // } else {
+  //   const { data, loading } = useQuery(getOneUser, { variables: { id } });
+  //   console.log(data);
+  // }
+
   return (
     <div className=" transition duration-500   h-96 bg-white bg-blur-xl bg-opacity-20 m-4 flex flex-col items-center shadow-inputShadow align-middle justify-between p-4 rounded-xl text-white transform hover:-translate-y-1 hover:scale-110">
       <Image width={130} height={230} src="/profile_placeholder.png" />
       <div className="w-full h-full flex flex-col items-left align-middle justify-center">
-        <div className="text-2xl">{name}</div>
-        <div className="text-xl">{business}</div>
-        <div className="text-xl">{job}</div>
-        <div className="text-xs">{description}</div>
+        <div className="text-2xl"></div>
+        <div className="text-xl"></div>
+        <div className="text-xl"></div>
+        <div className="text-xs"></div>
       </div>
       <div className="w-60">
-        <button className="text-sm font-bold bg-blue-400 rounded-xl px-4 py-1">Meet the Squad</button>
+        <button className="text-sm font-bold bg-blue-400 rounded-xl px-4 py-1">Fiverr Profil</button>
       </div>
     </div>
   );
--- components/Group_Card.tsx
+++ components/Group_Card.tsx
@@ -2,20 +2,24 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 interface IProps {
+  id: string;
   name: string;
   description: string;
 }
 
-export default function Group_Card({ name, description }: IProps) {
+export default function Group_Card({ id, name, description }: IProps) {
   return (
     <div className=" transition duration-500 w-11/12 sm:w-60 h-96 bg-white bg-blur-xl bg-opacity-20 m-4 flex flex-col items-center shadow-inputShadow align-middle justify-between p-4 rounded-xl text-white transform hover:-translate-y-1 hover:scale-110">
       <Image width={120} height={230} src="/profile_placeholder.png" />
       <div className="w-full h-full flex flex-col items-left align-middle justify-center">
         <div className="text-2xl">{name}</div>
-        <div className="text-xs">{description}</div>
+        <p className="text-xs h-20 overflow-ellipsis overflow-hidden">
+          {description}
+          {'...'}
+        </p>
       </div>
       <div className="w-full">
-        <Link href={`/squadProfil`}>
+        <Link href={`/squad/${id}`}>
           <button className="text-sm font-bold bg-blue-400 rounded-md px-4 py-1">Meet the Squad</button>
         </Link>
       </div>
