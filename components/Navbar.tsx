119fe53c3728d9a76051a1f443c3d684a1378ff1
viccabezos <cabezosvic@gmail.com>
2021-06-29 20:58:10 +0200

    link to home search

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,23 @@
-import Image from 'next/image';
-import Log from '../public/LogIn.svg';
-import SearchBar from './SearchBar';
-import burger from '../public/burger.svg';
-import { SetStateAction, Dispatch } from 'react';
-import { set } from 'react-hook-form';
+import Image from "next/image";
+import Log from "../public/LogIn.svg";
+import SearchBar from "./SearchBar";
+import burger from "../public/burger.svg";
+import { SetStateAction, Dispatch } from "react";
+import { set } from "react-hook-form";
 
-export default function Navbar({ setIsSideBarVisible }: { setIsSideBarVisible: Dispatch<SetStateAction<boolean>> }) {
+export default function Navbar({
+  setIsSideBarVisible,
+}: {
+  setIsSideBarVisible: Dispatch<SetStateAction<boolean>>;
+}) {
   const handleClick = () => {
     setIsSideBarVisible(true);
   };
   return (
     <div className="flex flex-col static sm:flex-row text-white sm:h-24 h-full font-Open z-20 px-3 sm:px-5 sm:py-6 py-5  justify-between  align-middle items-center w-full">
-      <div className="sm:text-4xl text-4xl font-bold mr-5">Fiverr. Squad</div>
+      <div className="sm:text-4xl text-4xl font-bold mr-5">
+        <a href="/squad">Fiverr. Squad</a>
+      </div>
       <SearchBar />
       <div className="flex items-end sm:items-center justify-between w-full sm:justify-end sm:w-2/12 px-2 mt-10 sm:mt-0">
         <div className="flex items-end sm:items-start">
@@ -19,7 +25,7 @@ export default function Navbar({ setIsSideBarVisible }: { setIsSideBarVisible: D
           <Image className="" src={Log} height={25} width={25} alt="log" />
         </div>
         <button className="focus:outline-none sm:hidden" onClick={handleClick}>
-          <Image src={burger} alt="burger" />{' '}
+          <Image src={burger} alt="burger" />{" "}
         </button>
       </div>
     </div>
