52e17dd5e158cb8bf752ad5fc26f6f09b5964b6e
Thomas <barrialthomas@gmail.com>
2021-06-30 13:19:01 +0200

    fix

--- pages/squad/[id].tsx
+++ pages/squad/[id].tsx
@@ -38,54 +38,42 @@ function squad() {
       <div className=" w-full text-white mb-40">
         <div className="flex justify-end mr-14">
           <Link href="/squad">
-            <button className="transition duration-500 bg-returnButton shadow-buttonShadow mt-5 px-8 py-2 rounded-md hover:scale-105">
-              Return
-            </button>
+            <button className="transition duration-500 bg-returnButton shadow-buttonShadow mt-5 px-8 py-2 rounded-md hover:scale-105">Return</button>
           </Link>
         </div>
         <div>
-          <h1 className="sm:text-7xl text-6xl mx-4 sm:mx-0 font-bold">
-            {squadData && squadData.Squad_by_pk.name}
-          </h1>
-          <div className="flex flex-row space-x-4 ">
-            <h2 className="mt-1 sm:text-base mx-4 sm:mx-0  ">
-              {squadData && squadData.Squad_by_pk.description}
-            </h2>
-
-            <button
-              onClick={() => setIsJoinForm(!isJoinForm)}
-              className="transition duration-500 bg-buttonBlue shadow-buttonShadow mt-1 px-8 py-2 rounded-md hover:-translate-y-1 h-16 hover:scale-105"
-            >
-              Join squad
-            </button>
-
-            <Link href="/hiresquad">
+          <h1 className="sm:text-7xl text-6xl mx-4 sm:mx-0 font-bold">{squadData && squadData.Squad_by_pk.name}</h1>
+          <div className="flex flex-col space-x-4 ">
+            <h2 className="mt-1 sm:text-base mx-4 pr-10 sm:mx-0  ">{squadData && squadData.Squad_by_pk.description}</h2>
+            <div className="mt-10">
               <button
-                className="ransition duration-500 h-16  bg-buttonBlue shadow-buttonShadow mt-1 px-8 py-2 rounded-md hover:-translate-y-1 hover:scale-105"
-                onClick={() => router.push('/hiresquad')}
-              >
-                Hire squad
+                onClick={() => setIsJoinForm(!isJoinForm)}
+                className="transition-10 mr-10 duration-500 bg-buttonBlue shadow-buttonShadow mt-1 px-8 py-2 rounded-md hover:-translate-y-1 h-12 hover:scale-105">
+                Join squad
               </button>
-            </Link>
+
+              <Link href="/hiresquad">
+                <button
+                  className="ransition duration-500 h-12  bg-buttonBlue shadow-buttonShadow mt-1 px-8 py-2 rounded-md hover:-translate-y-1 hover:scale-105"
+                  onClick={() => router.push('/hiresquad')}>
+                  Hire squad
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
 
         <div className=" flex w-full flex-col justify-center mt-5">
+          <h1 className="sm:text-3xl text-4xl mx-4 sm:mx-0 font-bold">Squad Freelancers</h1>
           <div className="flex flex-wrap ">
             {squadData &&
-              squadData?.Squad_by_pk?.users?.map(
-                (user: { User: IUser }, index: number) => {
-                  return <FreelanceCard key={user.User.id} id={user.User.id} />;
-                }
-              )}
+              squadData?.Squad_by_pk?.users?.map((user: { User: IUser }, index: number) => {
+                return <FreelanceCard key={user.User.id} id={user.User.id} />;
+              })}
           </div>
           <div className="flex mx-4 sm:mx-0 flex-col justify-center text-justify sm:w-10/12">
-            <h2 className="text-3xl font-bold mt-10 font-Open">
-              Squad Description
-            </h2>
-            <p className="sm:text-lg text-sm mt-2 font-Montserrat">
-              {squadData?.Squad_by_pk?.description}{' '}
-            </p>
+            <h2 className="text-3xl font-bold mt-10 font-Open">Squad Description</h2>
+            <p className="sm:text-lg text-sm mt-2 font-Montserrat">{squadData?.Squad_by_pk?.description} </p>
           </div>
         </div>
         <div className=" flex flex-col justify-center space-y-5 mt-14">
@@ -93,22 +81,11 @@ function squad() {
             The {squadData?.Squad_by_pk?.name} squad made this projects
           </h1>
           <div className="flex flex-wrap ">
-            {projectData?.Project?.map(
-              (
-                project: { squad_id: string; name: string; url: string },
-                index: number
-              ) => {
-                if (project.squad_id === squadData?.Squad_by_pk?.id) {
-                  return (
-                    <Project_Card
-                      key={index}
-                      name={project.name}
-                      url={project.url}
-                    />
-                  );
-                }
+            {projectData?.Project?.map((project: { squad_id: string; name: string; url: string }, index: number) => {
+              if (project.squad_id === squadData?.Squad_by_pk?.id) {
+                return <Project_Card key={index} name={project.name} url={project.url} />;
               }
-            )}
+            })}
           </div>
         </div>
       </div>
