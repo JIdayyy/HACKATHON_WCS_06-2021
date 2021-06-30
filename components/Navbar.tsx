5f88cedc98e151fb749fb473f879250f1607da34
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-30 13:40:04 +0200

    fix merge fail by JD

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,25 @@
-import Image from 'next/image';
-import Log from '../public/LogIn.svg';
-import SearchBar from './SearchBar';
-import burger from '../public/burger.svg';
-import { SetStateAction, Dispatch } from 'react';
-import Link from 'next/link';
-import { useRecoilState } from 'recoil';
-import { userState } from './states';
-import {useRouter} from 'next/router'
-import router from 'next/router';
-export default function Navbar({ setIsSideBarVisible }: { setIsSideBarVisible: Dispatch<SetStateAction<boolean>> }) {
+import Image from "next/image";
+import Log from "../public/LogIn.svg";
+import SearchBar from "./SearchBar";
+import burger from "../public/burger.svg";
+import { SetStateAction, Dispatch } from "react";
+import Link from "next/link";
+import { useRecoilState } from "recoil";
+import { userState } from "./states";
+import { useRouter } from "next/router";
+import router from "next/router";
+export default function Navbar({
+  setIsSideBarVisible,
+}: {
+  setIsSideBarVisible: Dispatch<SetStateAction<boolean>>;
+}) {
   const [myUser, setMyUser] = useRecoilState(userState);
   const router = useRouter();
 
   const handleClick = () => {
     setIsSideBarVisible(true);
     localStorage.clear();
-    router.push('/login');
+    router.push("/login");
   };
 
   return (
@@ -28,10 +32,19 @@ export default function Navbar({ setIsSideBarVisible }: { setIsSideBarVisible: D
       <div className="flex items-end sm:items-center justify-between w-full sm:justify-end sm:w-2/12 px-2 mt-10 sm:mt-0">
         <div className="flex flex-row-reverse justify-center sm:flex-row items-end sm:items-center">
           <Link href={`/profile/${myUser.id}`}>
-            <Image className="cursor-pointer" src={Log} height={25} width={25} alt="log" />
+            <Image
+              className="cursor-pointer"
+              src={Log}
+              height={25}
+              width={25}
+              alt="log"
+            />
           </Link>
 
-          <button onClick={handleClick} className="rounded-md w-24 h-9 mx-10 text-white bg-buttonBlue focus:outline-none shadow-inputShadow ">
+          <button
+            onClick={handleClick}
+            className="rounded-md w-24 h-9 mx-10 text-white bg-buttonBlue focus:outline-none shadow-inputShadow "
+          >
             <a href="/">Logout</a>
           </button>
         </div>
--- pages/squad/[id].tsx
+++ pages/squad/[id].tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import Project_Card from '../../components/Project_Card copy';
-import { useRouter } from 'next/dist/client/router';
-import { getOneSquad } from '../../apollo/squadQueries';
-import { allProjects } from '../../apollo/projectQueries';
-import { useQuery } from '@apollo/client';
-import Loading from '../../components/Loading';
-import { useState } from 'react';
-import JoinSquad from '../../components/JoinSquad';
-import Link from 'next/link';
-import FreelanceCard from '../../components/FreelanceCard2';
-import React from 'react';
-import Project_Card from '../../components/Project_Card copy';
-import { useRouter } from 'next/dist/client/router';
-import { getOneSquad } from '../../apollo/squadQueries';
-import { allProjects } from '../../apollo/projectQueries';
-import { useQuery } from '@apollo/client';
-import Loading from '../../components/Loading';
-import { useState } from 'react';
-import JoinSquad from '../../components/JoinSquad';
-import Link from 'next/link';
+import React from "react";
+import Project_Card from "../../components/Project_Card copy";
+import { useRouter } from "next/dist/client/router";
+import { getOneSquad } from "../../apollo/squadQueries";
+import { allProjects } from "../../apollo/projectQueries";
+import { useQuery } from "@apollo/client";
+import Loading from "../../components/Loading";
+import { useState } from "react";
+import JoinSquad from "../../components/JoinSquad";
+import FreelanceCard from "../../components/FreelanceCard2";
+
+import Link from "next/link";
 
 interface IUser {
   id: string;
@@ -47,24 +38,32 @@ function squad() {
       <div className=" w-full text-white mb-40">
         <div className="flex justify-end mr-14">
           <Link href="/squad">
-            <button className="transition duration-500 bg-returnButton shadow-buttonShadow mt-5 px-8 py-2 rounded-md hover:scale-105">Return</button>
+            <button className="transition duration-500 bg-returnButton shadow-buttonShadow mt-5 px-8 py-2 rounded-md hover:scale-105">
+              Return
+            </button>
           </Link>
         </div>
         <div>
-          <h1 className="sm:text-7xl text-6xl mx-4 sm:mx-0 font-bold">{squadData && squadData.Squad_by_pk.name}</h1>
+          <h1 className="sm:text-7xl text-6xl mx-4 sm:mx-0 font-bold">
+            {squadData && squadData.Squad_by_pk.name}
+          </h1>
           <div className="flex flex-col space-x-4 ">
-            <h2 className="mt-1 sm:text-base mx-4 pr-10 sm:mx-0  ">{squadData && squadData.Squad_by_pk.description}</h2>
+            <h2 className="mt-1 sm:text-base mx-4 pr-10 sm:mx-0  ">
+              {squadData && squadData.Squad_by_pk.description}
+            </h2>
             <div className="mt-10">
               <button
                 onClick={() => setIsJoinForm(!isJoinForm)}
-                className="transition-10 mr-10 duration-500 bg-buttonBlue shadow-buttonShadow mt-1 px-8 py-2 rounded-md hover:-translate-y-1 h-12 hover:scale-105">
+                className="transition-10 mr-10 duration-500 bg-buttonBlue shadow-buttonShadow mt-1 px-8 py-2 rounded-md hover:-translate-y-1 h-12 hover:scale-105"
+              >
                 Join squad
               </button>
 
               <Link href="/hiresquad">
                 <button
                   className="ransition duration-500 h-12  bg-buttonBlue shadow-buttonShadow mt-1 px-8 py-2 rounded-md hover:-translate-y-1 hover:scale-105"
-                  onClick={() => router.push('/hiresquad')}>
+                  onClick={() => router.push("/hiresquad")}
+                >
                   Hire squad
                 </button>
               </Link>
@@ -72,16 +71,24 @@ function squad() {
           </div>
         </div>
         <div className=" flex w-full flex-col justify-center mt-5">
-          <h1 className="sm:text-3xl text-4xl mx-4 sm:mx-0 font-bold">Squad Freelancers</h1>
+          <h1 className="sm:text-3xl text-4xl mx-4 sm:mx-0 font-bold">
+            Squad Freelancers
+          </h1>
           <div className="flex flex-wrap ">
             {squadData &&
-              squadData?.Squad_by_pk?.users?.map((user: { User: IUser }, index: number) => {
-                return <FreelanceCard key={user.User.id} id={user.User.id} />;
-              })}
+              squadData?.Squad_by_pk?.users?.map(
+                (user: { User: IUser }, index: number) => {
+                  return <FreelanceCard key={user.User.id} id={user.User.id} />;
+                }
+              )}
           </div>
           <div className="flex mx-4 sm:mx-0 flex-col justify-center text-justify sm:w-10/12">
-            <h2 className="text-3xl font-bold mt-10 font-Open">Squad Description</h2>
-            <p className="sm:text-lg text-sm mt-2 font-Montserrat">{squadData?.Squad_by_pk?.description} </p>
+            <h2 className="text-3xl font-bold mt-10 font-Open">
+              Squad Description
+            </h2>
+            <p className="sm:text-lg text-sm mt-2 font-Montserrat">
+              {squadData?.Squad_by_pk?.description}{" "}
+            </p>
           </div>
         </div>
         <div className=" flex flex-col justify-center space-y-5 mt-14">
@@ -89,11 +96,22 @@ function squad() {
             The {squadData?.Squad_by_pk?.name} squad made this projects
           </h1>
           <div className="flex flex-wrap ">
-            {projectData?.Project?.map((project: { squad_id: string; name: string; url: string }, index: number) => {
-              if (project.squad_id === squadData?.Squad_by_pk?.id) {
-                return <Project_Card key={index} name={project.name} url={project.url} />;
+            {projectData?.Project?.map(
+              (
+                project: { squad_id: string; name: string; url: string },
+                index: number
+              ) => {
+                if (project.squad_id === squadData?.Squad_by_pk?.id) {
+                  return (
+                    <Project_Card
+                      key={index}
+                      name={project.name}
+                      url={project.url}
+                    />
+                  );
+                }
               }
-            })}
+            )}
           </div>
         </div>
       </div>
